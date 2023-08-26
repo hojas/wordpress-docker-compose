6ec3be96fce371a0588cd06049180986fb1536ae
hojas <cyxhuan9@163.com>
2023-08-27 00:16:30 +0800

    ignore data

--- data/wordpress/wp-content/plugins/Importer/App/public/404.php
+++
@@ -1,4 +0,0 @@
-
-<div class="wrap" style="text-align: center;">
-    <img src="<?php echo plugin_dir_url(__FILE__); ?>/404_Image.png" alt="404 Page" style="margin-top: 2em; max-width: 100%;">
-</div>
